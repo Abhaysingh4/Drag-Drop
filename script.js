035b01609c0aea18a6f709669dbf0b22b76bbd24
AbhaySingh <tan63453@gmail.com>
2023-04-06 08:26:59 +0530

    added animation

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let sourceBox;
 let lastSwap;
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
   sourceBox = ev.target;
-  console.log(ev);
   ev.dataTransfer.setData("text", ev.target.id);
   ev.target.style.opacity = "0.4";
 }
@@ -22,7 +22,22 @@ function drop(ev) {
   sourceBox.style.backgroundColor = targetBox.style.backgroundColor;
   targetBox.innerHTML = tempContent;
   targetBox.style.backgroundColor = tempColor;
-  sourceBox.style.opacity = "1.0";
+
+  let sourceRect = sourceBox.getBoundingClientRect();
+  let targetRect = targetBox.getBoundingClientRect();
+  let deltaX = targetRect.left - sourceRect.left;
+  let deltaY = targetRect.top - sourceRect.top;
+  let animation = sourceBox.animate([
+    { transform: `translate(${deltaX}px, ${deltaY}px)` },
+    { transform: "none" }
+  ], {
+    duration: 700,
+    easing: "ease-out"
+  });
+
+  animation.onfinish = () => {
+    sourceBox.style.opacity = "1.0";
+  };
 }
 
 let boxes = document.querySelectorAll(".box");
@@ -33,22 +48,34 @@ for (let i = 0; i < boxes.length; i++) {
   box.addEventListener("dragover", allowDrop);
   box.addEventListener("drop", drop);
 }
-let undoButton = document.createElement("button");
-undoButton.textContent = "Undo last swap";
-undoButton.addEventListener("click", undoSwap);
-document.body.appendChild(undoButton);
+
 
 function undoSwap() {
-    if(lastSwap) {
-      let targetBox = lastSwap[0];
-      let sourceBox = lastSwap[1];
-      lastSwap = null;
-      let tempContent = sourceBox.innerHTML;
+  if (lastSwap) {
+    let targetBox = lastSwap[0];
+    let sourceBox = lastSwap[1];
+    lastSwap = null;
+    let tempContent = sourceBox.innerHTML;
     let tempColor = sourceBox.style.backgroundColor;
     sourceBox.innerHTML = targetBox.innerHTML;
     sourceBox.style.backgroundColor = targetBox.style.backgroundColor;
     targetBox.innerHTML = tempContent;
     targetBox.style.backgroundColor = tempColor;
-    }
+
+    let sourceRect = sourceBox.getBoundingClientRect();
+    let targetRect = targetBox.getBoundingClientRect();
+    let deltaX = targetRect.left - sourceRect.left;
+    let deltaY = targetRect.top - sourceRect.top;
+    let animation = sourceBox.animate([
+      { transform: `translate(${deltaX}px, ${deltaY}px)` },
+      { transform: "none" }
+    ], {
+      duration: 700,
+      easing: "ease-out"
+    });
+
+    animation.onfinish = () => {
+      sourceBox.style.opacity = "1.0";
+    };
   }
-  
+}
