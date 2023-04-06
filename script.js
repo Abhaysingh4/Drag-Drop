cc6e8b71d22f2ed4e24863f7672e73bf3e0fd79f
AbhaySingh <tan63453@gmail.com>
2023-04-06 08:00:49 +0530

    added undo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let sourceBox;
-
+let lastSwap;
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -14,6 +14,7 @@ function drag(ev) {
 function drop(ev) {
   ev.preventDefault();
   let targetBox = ev.target;
+  lastSwap = [sourceBox, targetBox];
   
   let tempContent = sourceBox.innerHTML;
   let tempColor = sourceBox.style.backgroundColor;
@@ -32,3 +33,22 @@ for (let i = 0; i < boxes.length; i++) {
   box.addEventListener("dragover", allowDrop);
   box.addEventListener("drop", drop);
 }
+let undoButton = document.createElement("button");
+undoButton.textContent = "Undo last swap";
+undoButton.addEventListener("click", undoSwap);
+document.body.appendChild(undoButton);
+
+function undoSwap() {
+    if(lastSwap) {
+      let targetBox = lastSwap[0];
+      let sourceBox = lastSwap[1];
+      lastSwap = null;
+      let tempContent = sourceBox.innerHTML;
+    let tempColor = sourceBox.style.backgroundColor;
+    sourceBox.innerHTML = targetBox.innerHTML;
+    sourceBox.style.backgroundColor = targetBox.style.backgroundColor;
+    targetBox.innerHTML = tempContent;
+    targetBox.style.backgroundColor = tempColor;
+    }
+  }
+  
